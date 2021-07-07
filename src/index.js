df3c082cc19939c617c28d4a89895992f0be3699
Rick <rmansfield@realpeoplesratings.com>
2021-07-07 09:29:42 -0500

    U2.1 M3 E Task 7 complete

--- src/index.js
+++ src/index.js
@@ -128,7 +128,12 @@ function makePanel(title, content) {
 
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
-
+  panel.classList.add('panel');
+  panelBar.classList.add('panel-bar');
+  panelButtons.classList.add('panel-Buttons');
+  openButton.classList.add('panel-btn-open');
+  closeButton.classList.add('panel-btn-close', 'hide-btn');
+  panelContent.classList.add('panel-content');
 
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
