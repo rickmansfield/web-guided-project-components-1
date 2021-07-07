cfdec5a09d8c3443b323c4cd96167372a72db1da
Rick <rmansfield@realpeoplesratings.com>
2021-07-07 09:13:04 -0500

    u2.1 m E gabs web guided task 7 complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const someData = ['foo', 'bar', 'baz', 'fizz']// this is raw data from a data server. 
-someData.forEach(str => {
-  const div = divMaker(str, 'grey');
-  document.body.prepend(div);
-});
+// someData.forEach(str => {
+//   const div1 = divMaker(str, 'grey');
+//   document.body.prepend(div1);
+// });
 
 function divMaker(text, color){// Component takes raw data, returns DOM element(s)
   const theNewDiv = document.createElement('div');//instantiation
@@ -12,10 +12,15 @@ function divMaker(text, color){// Component takes raw data, returns DOM element(
   return theNewDiv;//return
 }
 
+//React Versions
+// function divMaker(data){
+//   const { text, color } = data
+//   return <div style={{color: color}}>{text}</div>
+// }
 const div = divMaker('Lambda', 'red');
-document.body.prepend(div);
 const div2 = divMaker('web', 'green');
-document.body.prepend(div2);
+// document.body.prepend(div);
+// document.body.prepend(div2);
 
 
 // TASK 0- Motivate demoing a small makeImage component
@@ -27,53 +32,65 @@ const imageData = [
   { imageURL: 'https://images.dog.ceo/breeds/mastiff-bull/n02108422_2947.jpg' },
 ]
 //Image Creator Funtion
-function imageMaker(imageURL){
+function imageCreator(imgURL){
   let img = document.createElement('img');
-   img.src = imageURL;
+   img.src = imgURL;
   return img;
 }
 //React Version of the same fuction
 //return <img src={dara.src}/>
 //Variable to store images placed in the image Creator
-let images = imageData.map((i) => {
-  return imageMaker(i.imgageURL);
+let images = imageData.map((e) => {
+  return imageCreator(e.imageURL);
+  
 });
 //isolate place to put the newly created images & prend them there. 
 let secondary = document.querySelector('.secondary');
-images.forEach((img) => {
-  secondary.prepend(img);
-});
+// images.forEach((img) => {
+//   secondary.prepend(img);
+// });
 
 // TASK 1- Import the data we need to "hydrate" our component.
 //  On the one hand, the default export from data/panelData.js
 //  On the other hand, the default export from data/constants.js
 //  Destructure `open` and `close` from the constants
+import panelData from './data/panelData';
+import constants from './data/constants';
 
+console.log(`CONSTANTS`,constants);
+
+//Old way to imort
+// const open = constants.open;
+// const close = constants.close;
+
+//REACT Version -->> 
+const { open, close } = constants; //destructuring
 
 // TASK 2- Verify our imports using log statements
-console.log() // log the panelData
-console.log() // log the open arrow
-console.log() // log the close arrow
+console.log(`PANEL DATA`,panelData); // log the panelData
+console.log(`OPEN Arrow`, open); // log the open arrow
+console.log('CLOSE arrow', close); // log the close arrow
 
 
 // TASK 3- Comment out the div.panel from index.html and grab its parent element.
 //  We will generate the panel with code, and we'll need the parent
 //  so we can append the code-generated panel to the DOM.
-const accordion = null
+const accordion = document.querySelector('.accordion');
+console.log(accordion);
 
 
 // TASK 4- Create a function 'makePanel' that creates a panel exactly as you see it in the HTML.
-function makePanel(/* what data does the panel need? */) {
+function makePanel(title, content, open, close) {
 
 
   // TASK 5- Instantiate all the elements needed for a panel
-  const panel = null
-  const panelBar = null
-  const panelContent = null
-  const panelTitle = null
-  const panelButtons = null
-  const openButton = null
-  const closeButton = null
+  const panel = document.createElement('div');
+  const panelBar = document.createElement('div');
+  const panelContent = document.createElement('div');
+  const panelTitle = document.createElement('h3');
+  const panelButtons = document.createElement('h3');
+  const openButton = document.createElement('h3');
+  const closeButton = document.createElement('h3');
 
 
   // TASK 6- Setup the structure of our elements
@@ -89,11 +106,25 @@ function makePanel(/* what data does the panel need? */) {
       <div></div>           // panelContent
     </div>
   */
+//  accordion.appendChild(panelBar);
+ panel.appendChild(panelBar);
+ panelBar.appendChild(panelTitle);
+ panelBar.appendChild(panelButtons);
+ panelButtons.appendChild(openButton);
+ panelButtons.appendChild(closeButton);
+ panel.appendChild(panelContent);
+ 
 
 
   // TASK 7- Add proper class names to our elements (See index.html for reference)
   // paying attention to the elements that need to start out hidden
-
+  panel.classList.add('panel');
+  panelBar.classList.add('panel-bar');
+  panelButtons.classList.add('panel-Buttons');
+  openButton.classList.add('panel-btn-open');
+  closeButton.classList.add('panel-btn-close', 'hide-btn');
+  panelContent.classList.add('panel-content');
+  
 
   // TASK 8- Set text content using arguments as raw material
   //  and also using the open and close arrows imported at the top of the file
@@ -106,10 +137,10 @@ function makePanel(/* what data does the panel need? */) {
 
 
   // don't forget to return the panel!
-  return null
+  return panel;
 }
 
-
+console.log(makePanel('the title', 'the content'));
 // TASK 10- Loop through the panelData we imported from the data folder
 //  creating panels for each content and title and append them to the DOM.
 //  We can do this with a single forEach, or with a map and a forEach.
